d524e62eae2871c8bc3dccdec1c937b4280d1d2d
H.Gabriyel <thuppedagabriyel11@gmail.com>
2026-02-08 11:08:39 +0530

    first upload

---
+++ DBMS_programs/dbms.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE college1;
+USE colllege1;
+CREATE TABLE students(roll INT,name VARCHAR(20),branch VARCHAR(20));
+CREATE TABLE students VALUES(1,'RAVI','cse'),(2,'GABI','ECE');
+SELECT * FROM students;
